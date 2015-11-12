11cb24c89ea1673add774f5e7feae6f39b8c9758
Samson Brock <SamsonLB@gmail.com>
2015-11-11 21:19:54 -0700

    Add routes and create new poll views.
    Implement Express routes with EJS views to post and create new polls.

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-
 const path = require('path');
+const bodyParser = require('body-parser');
+const Poll = require('./lib/poll');
+
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res){
   res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/new', function (req, res){
+  res.sendFile(path.join(__dirname, '/public/new.html'));
+});
+
+app.post('/create', function (req, res){
+  var poll = new Poll();
+  poll.question = req.body.question;
+  poll.addChoice(req.body.choice1);
+  poll.addChoice(req.body.choice2);
+  poll.addChoice(req.body.choice3);
+
+  res.render('create', {
+    poll: poll
+  });
 });
 
 http.listen(process.env.PORT || 3000, function(){
