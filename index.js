4a67914a813ba0aba79fc1d5eb2ab88c5fdca6b2
androidgrl <kawaharajamie@gmail.com>
2015-11-12 14:06:19 -0700

    Add redis
    Sebastian showed me how to do this, it will
    save a stringified version of our poll object.

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const io = require('socket.io')(http);
 const path = require('path');
 const bodyParser = require('body-parser');
 const Poll = require('./lib/poll');
+const redis = require('redis');
+const client = redis.createClient();
 
+//check with redis-cli, keys *, hgetall "polls"
+//flushall
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
@@ -21,10 +25,12 @@ app.get('/new', function (req, res){
 
 app.post('/create', function (req, res){
   var poll = new Poll();
+
   poll.question = req.body.question;
   poll.addChoice(req.body.choice1);
   poll.addChoice(req.body.choice2);
   poll.addChoice(req.body.choice3);
+  client.hmset('polls', poll.id, JSON.stringify(poll));
 
   res.render('create', {
     poll: poll
@@ -34,6 +40,11 @@ app.post('/create', function (req, res){
 app.get('/vote/:id', function (req, res) {
   //get the pole object
   //display the choices and the question
+  //client.hgetall('polls', function(err, value){
+  // var poll = JSON.parse();
+  // to convert to JS object
+  //});
+  //redis will literally return a javascript object
   console.log(req.params.id);
 });
 
