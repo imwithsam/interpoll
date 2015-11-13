4b5ad53be1d6edf0402c3fb82d30d348ef48f3cd
androidgrl <kawaharajamie@gmail.com>
2015-11-12 21:26:07 -0700

    Add the vote results to the admin page
    However, this is not dynamically updated.
    It seems that application.js has inconsistent
    affect on the admin page.

--- public/application.js
+++ public/application.js
@@ -1,20 +1,19 @@
-// Initiate client-side WebSocket connection
 var socket = io();
 
 var voteCounts = document.getElementById('vote-counts');
 
-// Listen for emitted voteCount channel from server
 socket.on('voteCount', function(votes) {
-  var results = '';
+  console.log("=====votes======");
+  console.log(votes);
+  //var results = '';
 
-  for (var choice in votes) {
-    results = results + '<p>' + choice + ': ' + votes[choice] + '</p>';
-  };
+  //for (var choice in votes) {
+    //results = results + '<p>' + choice + ': ' + votes[choice] + '</p>';
+  //};
 
-  voteCounts.innerHTML = results;
+  //voteCounts.innerHTML = results;
 });
 
-// Add event listeners to all voting buttons
 var buttons = document.querySelectorAll('input[type="radio"]');
 
 for (var i = 0; i < buttons.length; i++) {
